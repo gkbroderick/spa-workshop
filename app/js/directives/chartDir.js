81b0a18f5aefbc12cfeaf34a4a638f0d4af2285c
Gabe Medrash <gmedrash@gmail.com>
2014-12-03 16:26:09 -0800

    tweak d3

--- app/js/directives/chartDir.js
+++ app/js/directives/chartDir.js
@@ -10,30 +10,31 @@ angular.module('directives').directive('barsChart', function($parse) {
       var watcher = function() {
         if (!scope.data) return;
 
-        var HEIGHT = 960;
-        var BAR_WIDTH = 48;
+        var HEIGHT = 500;
+        var BAR_WIDTH = 55;
 
         var x = d3.scale.linear()
             .domain([0, d3.max(scope.data, function(d) { return d.temperature; })])
             .range([0, HEIGHT]);
 
         var chart = d3.select(element[0])
-            .attr("width", BAR_WIDTH * scope.data.length)
-            .attr("height", HEIGHT);
+            .attr('width', BAR_WIDTH * scope.data.length)
+            .attr('height', HEIGHT);
 
-        var bar = chart.selectAll("g")
+        var bar = chart.selectAll('g')
             .data(scope.data)
-          .enter().append("g")
-            .attr("transform", function(d, i) { return 'translate(' + i * BAR_WIDTH + ", 0)"; });
+          .enter().append('g')
+            .attr('transform', function(d, i) { return 'translate(' + i * BAR_WIDTH + ', 0)'; });
 
-        bar.append("rect")
-            .attr("height", function(d) { return x(d.temperature); })
-            .attr("width", BAR_WIDTH - 1);
+        bar.append('rect')
+            .attr('height', function(d) { return x(d.temperature); })
+            .attr('y', function(d) { return HEIGHT - x(d.temperature); })
+            .attr('width', BAR_WIDTH - 1);
 
-        bar.append("text")
-            .attr("y", function(d) { return x(d.temperature) - 3; })
-            .attr("x", BAR_WIDTH / 2)
-            .attr("dy", ".35em")
+        bar.append('text')
+            .attr('y', function(d) { return HEIGHT - x(d.temperature) - 10; })
+            .attr('x', BAR_WIDTH / 2)
+            //.attr('dy', '.1em')
             .text(function(d) { return d.temperature; });
       }
 
